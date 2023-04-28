ad192c02af0ab1d32f7cb0b0d02583eee04f5b24
VincentAdamNemessis <126950990+VincentAdamNemessisX@users.noreply.github.com>
2023-04-28 15:23:24 +0800

    Add files via upload

--- handle/register.php
+++ handle/register.php
@@ -1,4 +1,19 @@
 <?php
+global $conn;
 session_start();
 date_default_timezone_set('PRC');
 include_once '../database/databaseHandle.php';
+$headPortrait = $_POST['headPortrait'];
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+if(insertData("accounts", [], [], ["username" => $username, "email" => $email,
+    "password" => md5($password), "headPortrait" => $headPortrait])) {
+    closeDatabase();
+    echo "<script>alert('Register Successful!');location.href='./index.php';</script>";
+} else {
+    closeDatabase();
+    $error = mysqli_error($conn);
+    echo "<script>alert('$error');location.history.back();</script>";
+}
+$_SESSION['user'] = $username;
